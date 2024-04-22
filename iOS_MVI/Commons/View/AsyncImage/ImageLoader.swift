0f952e7ffb3d138d3ee3d849a4ea39c372adaf1b
Hasan <serdarbakirtas@gmail.com>
2024-04-22 23:50:06 +0200

    Use actor to manage concurrent load image and Cancellation of image loading

--- iOS_MVI/Commons/View/AsyncImage/ImageLoader.swift
+++ iOS_MVI/Commons/View/AsyncImage/ImageLoader.swift
@@ -1,25 +1,42 @@
 import SwiftUI
 
-class ImageLoader: Observable {
-    var image: UIImage?
-
-    private var cache = NSCache<NSURL, UIImage>()
+actor ImageLoader: Observable {
+    private var image: UIImage?
+    private let cache = NSCache<NSURL, UIImage>()
+    private var currentTask: Task<Void, Error>?
 
     func loadImage(from url: URL) async {
-        if let cachedImage = cache.object(forKey: url as NSURL) {
+        // Cancel any ongoing task before starting a new one
+        currentTask?.cancel()
+
+        // Check cache
+        if let cachedImage = await cacheObject(forKey: url) {
             image = cachedImage
             return
         }
 
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            async let downloadedImage = UIImage(data: data)
-            guard let image = await downloadedImage else { return }
+            let downloadedImage = UIImage(data: data)
+            guard let image = downloadedImage else { return }
 
             cache.setObject(image, forKey: url as NSURL)
             self.image = image
         } catch {
-            Logger.error("Failed to load image: \(error)")
+            // Handle cancellation error separately
+            if let cancellationError = error as? CancellationError {
+                Logger.info("Image loading cancelled.")
+            } else {
+                Logger.error("Failed to load image: \(error)")
+            }
         }
+    }
+
+    private func cacheObject(forKey key: URL) async -> UIImage? {
+        cache.object(forKey: key as NSURL)
+    }
+
+    func cancelLoad() {
+        currentTask?.cancel()
     }
 }
