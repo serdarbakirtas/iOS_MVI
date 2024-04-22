c7b5b7dabcde47368a5f4a3621afc60d4a3ec3e0
Hasan <serdarbakirtas@gmail.com>
2024-04-22 16:41:45 +0200

    Use CacheAsyncImage instead of AsyncImage

--- iOS_MVI/Commons/View/CircularImageView.swift
+++ iOS_MVI/Commons/View/CircularImageView.swift
@@ -4,12 +4,23 @@ struct CircularImageView: View {
     let imageURL: URL?
 
     var body: some View {
-        AsyncImage(url: imageURL)
-            .frame(width: 160, height: 160)
-            .aspectRatio(contentMode: .fill)
-            .clipShape(Circle())
-            .overlay(Circle().stroke(Color.white, lineWidth: 2))
-            .shadow(radius: 5)
+        profileImage
+    }
+}
+
+// MARK: Child views
+
+extension CircularImageView {
+    @ViewBuilder
+    private var profileImage: some View {
+        if let imageURL {
+            CacheAsyncImage(url: imageURL)
+                .frame(width: 160, height: 160)
+                .aspectRatio(contentMode: .fill)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .shadow(radius: 5)
+        }
     }
 }
 
